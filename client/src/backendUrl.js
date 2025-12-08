5d384dbd4871821d77d27844f792ec2cfb88c888
shiva171717 <115244540+shiva171717@users.noreply.github.com>
2025-12-08 16:31:55 +0530

    Update BASE_URL to new production endpoint

--- client/src/backendUrl.js
+++ client/src/backendUrl.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "https://ecommerce-8w8d.onrender.com";
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://military-personnel-database-management.onrender.com";
+
